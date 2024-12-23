938dde0bea6a73e7fca0cb4b0c1f7a5d90064d1d
farhanelta <farhanelta4@gmail.com>
2024-12-23 21:30:28 +0700

    COMPANY SETUP IN PAGE.JS

--- src/app/page.js
+++ src/app/page.js
@@ -1,163 +1,33 @@
 "use client";
-
-export default function Home() {
+import TopNavigaton from "@/components/topbar";
+export default function AboutPage() {
   return (
     <div className="min-w-screen min-h-screen bg-white flex flex-col">
-      <nav className="bg-white border-b border-gray-200 sticky top-0">
-        <div className="flex flex-wrap place-items-center justify-between p-4">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
-              Interners
-            </span>
-          </a>
 
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-controls="navbar-default"
-            aria-expanded="false"
-            onClick={() => {
-              const menu = document.getElementById("navbar-default");
-              menu.classList.toggle("hidden");
-            }}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 place-items-center md:flex-row md:space-x-4 rtl:space-x-reverse">
-              <li>
-                <a
-                  href="#"
-                  className="font-medium text-black hover:text-blue-900"
-                >
-                  Contact
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="font-medium text-black hover:text-blue-900"
-                >
-                  About Us
-                </a>
-              </li>
-              <li>
-                <button className="border border-gray-400 rounded-full text-black hover:text-blue-900 hover:border-blue-900">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-6 m-1"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </li>
-              <li>
-                <button className="border border-gray-400 rounded-full text-black hover:text-blue-900 hover:border-blue-900">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-6 m-1"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </li>
-              <li>
-                <button className="border border-gray-400 rounded-full text-black hover:text-blue-900 hover:border-blue-900">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="size-6 m-1"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 0 1-.517.608 7.45 7.45 0 0 0-.478.198.798.798 0 0 1-.796-.064l-.453-.324a1.875 1.875 0 0 0-2.416.2l-.243.243a1.875 1.875 0 0 0-.2 2.416l.324.453a.798.798 0 0 1 .064.796 7.448 7.448 0 0 0-.198.478.798.798 0 0 1-.608.517l-.55.092a1.875 1.875 0 0 0-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 0 1-.064.796l-.324.453a1.875 1.875 0 0 0 .2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 0 1 .796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 0 1 .517-.608 7.52 7.52 0 0 0 .478-.198.798.798 0 0 1 .796.064l.453.324a1.875 1.875 0 0 0 2.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 0 1-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 0 0 1.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 0 1-.608-.517 7.507 7.507 0 0 0-.198-.478.798.798 0 0 1 .064-.796l.324-.453a1.875 1.875 0 0 0-.2-2.416l-.243-.243a1.875 1.875 0 0 0-2.416-.2l-.453.324a.798.798 0 0 1-.796.064 7.462 7.462 0 0 0-.478-.198.798.798 0 0 1-.517-.608l-.091-.55a1.875 1.875 0 0 0-1.85-1.566h-.344ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      <main className="flex flex-col min-w-screen">
-        <div className="grow">
-          <div className="mb-24">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYXdO7mK7s8qsbgK13AaUbCo8Wo122wEzROw&s"
-              alt="Girl in a jacket"
-              className="w-screen h-48 object-cover"
-            />
-            <img
-              src="https://cdn2.hubspot.net/hubfs/53/image8-2.jpg"
-              className="rounded-full bg-white w-32 h-32 object-cover absolute top-60 left-12"
-            />
-          </div>
-          <div className="mx-6">
-            <p className="text-3xl font-bold text-black mb-2">Google Corp.</p>
-            <p className="text-base text-black mb-4">New York, America</p>
-            <textarea
-              id="message"
-              rows="4"
-              class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Company description..."
-            ></textarea>
-          </div>
-          <div className="bg-gray-400 h-[0.5px] min-w-screen my-6"></div>
-          <div className="mx-6 mb-6">
-            <div className="flex justify-between">
-              <p className="text-xl text-black font-semibold mb-2">
-                Job Listing
-              </p>
-              <a>
-                <p className="text-gray-400 hover:text-blue-700">See All</p>
-              </a>
-            </div>
-            <p className="text-base text-black mb-2">
-              There is no listing at the moment
+      <TopNavigaton />
+      
+      <div className="grow flex justify-center items-center h-100 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% py-[96]">
+        <div className="place-items-center">
+          <div>
+            <p className="font-semibold text-4xl text-black">
+              NICE TO MEET YOU, FUTURE EMPLOYER!
             </p>
-            <a>
-              <div className="rounded-xl border border-gray-400 border-dashed hover:border-solid w-48 h-32 place-items-center content-center">
-                <p className="text-3xl text-gray-400 font-semibold">+</p>
-              </div>
+          </div>
+          <div className="mt-4 place-items-center">
+            <p className="font-regular text-2xl text-black">
+              Looks like you haven’t set up your company profile
+            </p>
+            <p className="font-regular text-2xl text-black">
+              Let’s get you started, don’t we?
+            </p>
+          </div>
+          <div className="mt-12">
+            <a className="font-semibold bg-white rounded-md p-2 text-black ">
+              GET STARTED
             </a>
           </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 }
